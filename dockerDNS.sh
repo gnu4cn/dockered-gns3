b5e927c0406eae94b29356fc63af51127e4d5111
gnu4cn <unisko@gmail.com>
2015-05-04 12:33:05 +0800

    移除 dockerDNS.sh 脚本，将其修改 docker 配置文件 /etc/default/docker 功能并入 mybuild.sh 脚本

--- dockerDNS.sh
+++
@@ -1,1 +0,0 @@
-echo 'DOCKER_OPTS="--dns 192.168.20.6 --dns 221.6.4.66"' >> /etc/default/docker
--- mybuild.sh
+++ mybuild.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-sudo docker build -t="unisko/gns3-dockered" .
+echo 'DOCKER_OPTS="--dns 192.168.20.6 --dns 221.6.4.66"' >> /etc/default/docker
+docker build -t="unisko/gns3-dockered" .
